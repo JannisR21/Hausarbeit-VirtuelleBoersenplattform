38d97fbad1969e1c9d571178fcc098b24beab8f6
Jannis <Jannisr@kabelmail.de>
2025-04-26 15:44:30 +0200

    LiveChart-Komponente für Portfolio-Visualisierung hinzugefügt

---
+++ HausarbeitVirtuelleBörsenplattform/HausarbeitVirtuelleBörsenplattform/Views/PortfolioChartControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HausarbeitVirtuelleBörsenplattform.Views
+{
+    /// <summary>
+    /// Interaktionslogik für PortfolioChartControl.xaml
+    /// </summary>
+    public partial class PortfolioChartControl : UserControl
+    {
+        public PortfolioChartControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

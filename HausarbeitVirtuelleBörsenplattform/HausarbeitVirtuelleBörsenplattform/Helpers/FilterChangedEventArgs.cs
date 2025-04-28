3887813af1c91b9c0194c0ef90f50f0ebb5c6625
Jannis <Jannisr@kabelmail.de>
2025-04-28 21:04:01 +0200

    Event-Args für Filter-Kommunikation zwischen Komponenten hinzugefügt

---
+++ HausarbeitVirtuelleBörsenplattform/HausarbeitVirtuelleBörsenplattform/Helpers/FilterChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using HausarbeitVirtuelleBörsenplattform.Models;
+
+namespace HausarbeitVirtuelleBörsenplattform.Helpers
+{
+    public class FilterChangedEventArgs : EventArgs
+    {
+        public IEnumerable<Aktie> GefilterteAktien { get; }
+        public string SuchText { get; }
+        public bool IstFilterAktiv { get; }
+
+        public FilterChangedEventArgs(IEnumerable<Aktie> gefilterteAktien, string suchText, bool istFilterAktiv)
+        {
+            GefilterteAktien = gefilterteAktien;
+            SuchText = suchText;
+            IstFilterAktiv = istFilterAktiv;
+        }
+    }
+}

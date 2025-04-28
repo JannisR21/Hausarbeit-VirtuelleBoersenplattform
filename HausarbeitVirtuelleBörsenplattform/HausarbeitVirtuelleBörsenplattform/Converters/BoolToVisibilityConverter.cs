e77e56d23a315c4e8fe0c98dcf6fcb0ced440630
Jannis <Jannisr@kabelmail.de>
2025-04-28 21:03:42 +0200

    Neue Converter-Klassen für UI-Binding hinzugefügt

---
+++ HausarbeitVirtuelleBörsenplattform/HausarbeitVirtuelleBörsenplattform/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HausarbeitVirtuelleBörsenplattform.Converters
+{
+    /// <summary>
+    /// Konvertiert einen Boolean-Wert in einen Visibility-Wert und umgekehrt
+    /// </summary>
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Konvertiert einen Boolean-Wert in einen Visibility-Wert
+        /// </summary>
+        /// <param name="value">Der zu konvertierende Wert</param>
+        /// <param name="targetType">Der Zieltyp</param>
+        /// <param name="parameter">Ein Parameter, mit dem ein Inverted-Verhalten gesteuert werden kann</param>
+        /// <param name="culture">Die Kultur</param>
+        /// <returns>Visibility.Visible wenn true, sonst Visibility.Collapsed (oder umgekehrt bei Parameter=inverted)</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isInverted = parameter != null && parameter.ToString().ToLower() == "inverted";
+            bool boolValue = value != null && (bool)value;
+
+            if (isInverted)
+                boolValue = !boolValue;
+
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Konvertiert einen Visibility-Wert zurück in einen Boolean-Wert
+        /// </summary>
+        /// <param name="value">Der zu konvertierende Wert</param>
+        /// <param name="targetType">Der Zieltyp</param>
+        /// <param name="parameter">Ein Parameter, mit dem ein Inverted-Verhalten gesteuert werden kann</param>
+        /// <param name="culture">Die Kultur</param>
+        /// <returns>true wenn Visibility.Visible, sonst false (oder umgekehrt bei Parameter=inverted)</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isInverted = parameter != null && parameter.ToString().ToLower() == "inverted";
+            Visibility visibility = (Visibility)value;
+            bool result = visibility == Visibility.Visible;
+
+            if (isInverted)
+                result = !result;
+
+            return result;
+        }
+    }
+}
---
+++ HausarbeitVirtuelleBörsenplattform/HausarbeitVirtuelleBörsenplattform/Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HausarbeitVirtuelleBörsenplattform.Converters
+{
+    /// <summary>
+    /// Konvertiert einen Enum-Wert in einen Boolean-Wert und umgekehrt
+    /// </summary>
+    public class EnumToBooleanConverter : IValueConverter
+    {
+        /// <summary>
+        /// Konvertiert einen Enum-Wert zu einem Boolean
+        /// </summary>
+        /// <param name="value">Der zu konvertierende Wert</param>
+        /// <param name="targetType">Der Zieltyp</param>
+        /// <param name="parameter">Der Enum-Wert, der mit value verglichen wird</param>
+        /// <param name="culture">Die Kultur</param>
+        /// <returns>true wenn value und parameter gleich sind, sonst false</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            // Wenn value null nullable ist
+            if (value is bool boolValue)
+            {
+                bool paramValue;
+                if (parameter is bool)
+                {
+                    paramValue = (bool)parameter;
+                }
+                else if (parameter is string && bool.TryParse((string)parameter, out bool parsedBool))
+                {
+                    paramValue = parsedBool;
+                }
+                else
+                {
+                    return false;
+                }
+
+                return boolValue == paramValue;
+            }
+
+            string valueStr = value.ToString();
+            string parameterStr = parameter.ToString();
+
+            return valueStr.Equals(parameterStr, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Konvertiert einen Boolean-Wert zurück zu einem Enum-Wert
+        /// </summary>
+        /// <param name="value">Der zu konvertierende Wert</param>
+        /// <param name="targetType">Der Zieltyp</param>
+        /// <param name="parameter">Der Enum-Wert, der zurückgegeben wird, wenn value true ist</param>
+        /// <param name="culture">Die Kultur</param>
+        /// <returns>Den Enum-Wert, wenn value true ist, sonst den Standard-Enum-Wert</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null || !(value is bool))
+                return null;
+
+            bool boolValue = (bool)value;
+
+            if (!boolValue)
+                return null;
+
+            // Wenn targetType bool ist
+            if (targetType == typeof(bool) || targetType == typeof(bool?))
+            {
+                if (parameter is bool)
+                {
+                    return parameter;
+                }
+                else if (parameter is string && bool.TryParse((string)parameter, out bool parsedBool))
+                {
+                    return parsedBool;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            // Wenn targetType ein Enum ist
+            if (targetType.IsEnum)
+            {
+                return Enum.Parse(targetType, parameter.ToString());
+            }
+
+            return parameter;
+        }
+    }
+}

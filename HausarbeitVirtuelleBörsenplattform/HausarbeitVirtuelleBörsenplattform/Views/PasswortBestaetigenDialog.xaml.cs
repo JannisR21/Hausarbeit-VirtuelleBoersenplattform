46a0bd52098ad3e43b207b16b510142083dbe007
Jannis <Jannisr@kabelmail.de>
2025-05-08 10:09:20 +0200

    Füge Code-Behind für Passwort-Bestätigungsdialog hinzu
      - Implementiere Event-Handler für Dialog-Interaktionen
      - Sicheres Handling von Passwort-Eingaben
      - Validierung von Benutzereingaben mit Feedback
      - Ordnungsgemäße Dialog-Schließung mit Ergebnisübermittlung

---
+++ HausarbeitVirtuelleBörsenplattform/HausarbeitVirtuelleBörsenplattform/Views/PasswortBestaetigenDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace HausarbeitVirtuelleBörsenplattform.Views
+{
+    /// <summary>
+    /// Interaktionslogik für PasswortBestaetigenDialog.xaml
+    /// </summary>
+    public partial class PasswortBestaetigenDialog : Window
+    {
+        /// <summary>
+        /// Das eingegebene Passwort
+        /// </summary>
+        public string Password => PasswordBox.Password;
+
+        /// <summary>
+        /// Initialisiert einen neuen PasswortBestaetigenDialog
+        /// </summary>
+        public PasswortBestaetigenDialog()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event-Handler für den Abbrechen-Button
+        /// </summary>
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        /// <summary>
+        /// Event-Handler für den Bestätigen-Button
+        /// </summary>
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Bitte geben Sie Ihr Passwort ein.",
+                    "Passwort fehlt", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            DialogResult = true;
+            Close();
+        }
+    }
+}
